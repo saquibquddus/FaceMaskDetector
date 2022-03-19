a1c52876005f15175cb542b50cb30e4248962f88
saquibquddus <sakibquddus@gmail.com>
2022-03-19 09:15:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Face Mask Detection Demo
+Face Mask Detection
+
---
+++ clientApp.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify, render_template
+import os
+from flask_cors import CORS, cross_origin
+from com_in_ineuron_ai_utils.utils import decodeImage
+from research.obj import MultiClassObj
+
+# os.putenv('LANG', 'en_US.UTF-8')
+# os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+
+
+# @cross_origin()
+class ClientApp:
+    def __init__(self):
+        self.filename = "inputimage.png"
+        modelPath = 'research/faster_rcnn'
+        self.objectDetection = MultiClassObj(self.filename, modelPath)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    #clApp = ClientApp()
+    image = request.json['image']
+    decodeImage(image, clApp.filename)
+    result = clApp.objectDetection.getPrediction()
+    return jsonify(result)
+
+
+#port = int(os.getenv("PORT"))
+if __name__ == "__main__":
+    clApp = ClientApp()
+    port = 5000
+    app.run(host='127.0.0.1', port=port)
+    #app.run(host='0.0.0.0', port=7000, debug=True)
